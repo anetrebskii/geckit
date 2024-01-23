6f2426ddaf69bf543c83b4a616d8b42eb0591a52
anetrebsky <aleks.netrebskiy@gmail.com>
2024-01-23 08:08:24 +0400

    Fix tests

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,9 +1,21 @@
 import '@testing-library/jest-dom';
+import 'openai/shims/node';
 import { render } from '@testing-library/react';
 import App from '../renderer/App';
+// import { MyIpcRenderer } from '../main/preload';
 
 describe('App', () => {
   it('should render', () => {
+    const mockIpcRenderer = {
+      on: jest.fn(),
+      sendMessage: jest.fn(),
+      once: jest.fn(),
+    };
+
+    window.electron = {
+      ipcRenderer: mockIpcRenderer,
+    };
+
     expect(render(<App />)).toBeTruthy();
   });
 });
