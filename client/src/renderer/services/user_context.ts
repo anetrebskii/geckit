da4d3e3b857dba79cc73a359615d8ce98a67182f
anetrebsky <aleks.netrebskiy@gmail.com>
2024-02-10 12:45:57 +0400

    Fix version for windows

--- client/src/renderer/services/user_context.ts
+++ client/src/renderer/services/user_context.ts
@@ -28,37 +28,14 @@ export function getUserContext(): UserContext {
     return DefaultUserContext;
   }
 
-  const result: UserContext = JSON.parse(userContextText);
-
-  // Backward compartibility
-  result.settings = {
-    nativateLanguage:
-      window.localStorage.getItem('lang1') ?? result.settings.nativateLanguage,
-    secondLanguage:
-      window.localStorage.getItem('lang2') ?? result.settings.secondLanguage,
-    openAiKey:
-      window.localStorage.getItem('openApi') ?? result.settings.openAiKey,
-    openAiModel:
-      result.settings?.openAiModel ?? DefaultUserContext.settings.openAiModel,
-  };
-
+  const result: UserContext = {
+    ...DefaultUserContext,
+    ...JSON.parse(userContextText)
+  }
   return result;
 }
 
 export function setUserContext(userContext: Partial<UserContext>) {
-  // Backward compartibility
-  if (userContext.settings?.nativateLanguage) {
-    window.localStorage.removeItem('lang1');
-  }
-
-  if (userContext.settings?.secondLanguage) {
-    window.localStorage.removeItem('lang2');
-  }
-
-  if (userContext.settings?.openAiKey) {
-    window.localStorage.removeItem('openApi');
-  }
-
   window.localStorage.setItem(
     StorageKey,
     JSON.stringify({
